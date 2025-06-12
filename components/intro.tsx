7e6401cc16fca8eb8dc8bb5b0bb28564cf9ec312
Abhishek Jatav <abhidel44@gmail.com>
2025-06-12 20:09:55 +0530

    test

--- components/intro.tsx
+++ components/intro.tsx
@@ -34,7 +34,7 @@ export default function Intro() {
               width={192}
               height={192}
               quality={95}
-              loading="lazy"
+              priority={true}
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
